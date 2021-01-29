5f3c5966b324eac421f3d590e25b8a899aa6101f
Ismael_T <57118740+ismael17-bot@users.noreply.github.com>
2021-01-29 11:50:15 -0300

    Add files via upload

---
+++ funvetores.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+    <?php
+    $vetor2=range(1,20,1);
+    var_dump($vetor2);
+    echo" o vetor tem ".count($vetor2); 
+    /*
+    array_pop(); elimina o ultimo elemento
+    array_push();adiciona um elemento
+    
+    */
+    //array_unshift()inclui um elemento no inicio do vetor
+    //array_shift()exclui o primeiro elemento e atualiza as posiçoes
+    //sort() organiza em ordem numerica
+    //rsort() organiza em ordem inversa
+    //ksort() coloca os inde em ordem
+
+    ?>
+    </pre>
+</body>
+</html>
